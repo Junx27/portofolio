58df8246b7381cbb40c6aaa2d702f04bf480ebb2
Junx27 <junxitsumo@gmail.com>
2023-10-09 14:30:33 +0700

    test pretier

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <h1>test hello my junx</h1>
-  )
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sint
+        ipsam corporis vel vero officiis, earum accusantium dignissimos iusto
+        inventore culpa aut tempora nulla deleniti, quia quod modi consequuntur
+        aspernatur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro nihil
+        quis itaque aspernatur tenetur quidem reprehenderit possimus delectus
+        sit cumque? Repellendus porro corrupti eligendi voluptas consectetur,
+        modi veritatis ea quasi!
+      </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
